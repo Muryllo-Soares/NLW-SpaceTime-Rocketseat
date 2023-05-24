afb11f130b41749e9b54702286cb73792da60c1f
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-05-24 17:18:51 -0300

    atualização aplicação mobile

--- capsula-do-tempo/mobile/app/index.tsx
+++ capsula-do-tempo/mobile/app/index.tsx
@@ -1,63 +1,43 @@
-import { StatusBar } from 'expo-status-bar'
-import { useEffect } from 'react'
-import { useRouter } from 'expo-router'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { styled } from 'nativewind'
-import * as SecureStore from 'expo-secure-store'
+import { useEffect } from "react";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import * as SecureStore from "expo-secure-store";
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
-import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { api } from '../src/lib/api'
-
-const StyledStripes = styled(Stripes)
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
+import { api } from "../src/lib/api";
 
 const discovery = {
-  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
   revocationEndpoint:
-    'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
-}
+    "https://github.com/settings/connections/applications/d26f194cc5d5132a51be",
+};
 
 export default function App() {
-  const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
+  const router = useRouter();
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: 'd26f194cc5d5132a51be',
-      scopes: ['identity'],
+      clientId: "353018d996aee5e7cf12",
+      scopes: ["identity"],
       redirectUri: makeRedirectUri({
-        scheme: 'nlwspacetime',
+        scheme: "nlwspacetime",
       }),
     },
-    discovery,
-  )
+    discovery
+  );
 
   async function handleGithubOAuthCode(code: string) {
-    const response = await api.post('/register', {
+    const response = await api.post("/register", {
       code,
-    })
+    });
 
-    const { token } = response.data
+    const { token } = response.data;
 
-    await SecureStore.setItemAsync('token', token)
+    await SecureStore.setItemAsync("token", token);
 
-    router.push('/memories')
+    router.push("/memories");
   }
 
   useEffect(() => {
@@ -68,25 +48,15 @@ export default function App() {
     //   }),
     // )
 
-    if (response?.type === 'success') {
-      const { code } = response.params
+    if (response?.type === "success") {
+      const { code } = response.params;
 
-      handleGithubOAuthCode(code)
+      handleGithubOAuthCode(code);
     }
-  }, [response])
-
-  if (!hasLoadedFonts) {
-    return null
-  }
+  }, [response]);
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -114,8 +84,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito por Muryllo Soares
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
-  )
+    </View>
+  );
 }
--- capsula-do-tempo/web/src/app/layout.tsx
+++ capsula-do-tempo/web/src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Hero } from "@/components/Hero";
 import { Profile } from "@/components/Profile";
 import { SignIn } from "@/components/SignIn";
 import { Copyright } from "@/components/Copyright";
-import {cookies} from 'next/headers'
+import { cookies } from "next/headers";
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 const baiJamjuree = BaiJamjuree({
@@ -19,33 +19,36 @@ const baiJamjuree = BaiJamjuree({
 
 export const metadata = {
   title: "NLW SpaceTime",
-  description: "Uma Cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript ",
+  description:
+    "Uma Cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript ",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const isAuthenticated = cookies().has('token')
+  const isAuthenticated = cookies().has("token");
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
         <main className="grid min-h-screen grid-cols-2">
-      {/* Section Left */}
-      <div className="relative flex flex-col items-center justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        {/* Blur*/}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+          {/* Section Left */}
+          <div className="relative flex flex-col items-center justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur*/}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
-        {/* Stripes*/}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            {/* Stripes*/}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
 
-      {/* Section Right */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-      {children}
-      </div>
-    </main>
+          {/* Section Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
